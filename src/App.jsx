dc90bc5cf65e9dd44519834f794193450ca582c3
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-09-25 17:57:46 +0300

    updated now

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+import { useState } from "react";
+import ToDoList from "./ToDoList.jsx"; // Ensure the path is correct
+
+function App() {
+  return (
+    <>
+      (<ToDoList />) {/* Render the ToDoList component */}
+    </>
+  );
+}
+
+export default App;
